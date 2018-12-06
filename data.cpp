5e702fcba0c07a399b78d40dfcc058c6fcfe9bcc
ekawati0706 <43712502+ekawati0706@users.noreply.github.com>
2018-12-06 22:33:49 +0700

    Add files via upload

---
+++ data.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+
+using namespace std;
+typedef struct
+{ string no[15],nama[25],posisi[25];
+	int atas=0; int bawah=1;
+    }stack_player;
+stack_player player;
+bool emptyStack(stack_player X){ if(X.atas==0) {return true;} else {return false;}}
+bool fullStack(stack_player X){if(X.atas==100){return true;} else{return false;}}
+
+
+void Push_Stack(stack_player &X)
+{
+	X.atas++;
+	cout<<" "<<endl;
+	cout<<"DATA PEMAIN SEPAK BOLA "<<endl;
+	cout<<"------------------------------"<<endl;
+	cout<<"No.punggung      : ";cin>>X.no[X.atas];
+	cout<<"Nama Pemain      : ";cin>>X.nama[X.atas];
+	cout<<"Posisi		 	: ";cin>>X.posisi[X.atas];
+
+	cout<<" "<<endl;
+
+} 
+void Print_Stack(stack_player &X)
+{
+cout<<"Isi pada Stack"<<endl;
+cout<<"------------------------------"<<endl;	
+for(int i=X.atas;i>=1;i--)
+cout<<X.no[i]<<" -> "<<X.nama[i]<<" -> "<<X.posisi[i]<<endl;
+
+}
+
+void Pop_Stack(stack_player &X)
+{
+	X.atas--;
+}
+
+
+int main()
+{	int pilih=0;
+	do
+	{
+
+	cout<<"------------------------------------------------------------------------------"<<endl;
+	cout<<" 			STACK(TUMPUKAN) MENGGUNAKAN LINKED LIST"<<endl;
+	cout<<" "<<endl;
+	cout<<"1:PUSH"<<endl;
+	cout<<"2:POP"<<endl;
+	cout<<"3:PRINT"<<endl;
+	cout<<"4:Keluar"<<endl;
+
+	cout<<"Pilih (1-4): ";cin>>pilih;
+	switch(pilih){
+case 1:{if(fullStack(player)){cout<<"Stack penuh!"<<endl;}
+	   	    else {Push_Stack(player);}break;}
+case 2:{if(emptyStack(player)){cout<<"Stack kosong!"<<endl;}
+		 	  else {Pop_Stack(player);}break;}
+
+case 3:{if(emptyStack(player)){cout<<"Stack kosong!"<<endl;}
+			else {Print_Stack(player);}break;}	
+
+case 4:{
+	return 0;
+	break;
+}
+
+}
+   
+}
+ while(pilih!=0);
+}
+
+
